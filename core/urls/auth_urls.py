a42cbe6fa1dd745dbd24dbca6b9662b834e12765
MAC <mac@MACs-MacBook-Air-2.local>
2025-07-10 12:44:01 +0100

    Pulled /core from dev branch

--- core/urls/auth_urls.py
+++ core/urls/auth_urls.py
@@ -9,7 +9,7 @@ from django.urls import path
 from ..views.auth_views import (
     RegisterView, LoginView, LogoutView, UserProfileView,
     PasswordResetRequestView, PasswordResetConfirmView,
-    ChangePasswordView, RefreshTokenView
+    ChangePasswordView, RefreshTokenView, StudentProtectedDummyView
 )
 
 urlpatterns = [
@@ -26,4 +26,6 @@ urlpatterns = [
     path('password/reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password/change/', ChangePasswordView.as_view(), name='change_password'),
-] 
+
+    path('student-protected/', StudentProtectedDummyView.as_view(), name='student_protected'),
+]
